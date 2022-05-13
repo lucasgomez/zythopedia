87377ae0a3bdb667523eff1bd555406d44a896d6
Lucas Gomez <lucas.gomez@vd.ch>
2022-05-13 11:44:19 +0200

    Initial commit angular empty app, springboot empty app and okta basics

---
+++ src/main/java/ch/fdb/zythopedia/ZythopediaApplication.java
@@ -0,0 +1,13 @@
+package ch.fdb.zythopedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZythopediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZythopediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/fdb/zythopedia/ZythopediaApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.fdb.zythopedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZythopediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
