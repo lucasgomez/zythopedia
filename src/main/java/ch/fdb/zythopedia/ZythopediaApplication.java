c6c5bcf60e1adf425f460a222b16249ea5563455
Lucas Gomez <lucas.gomez@vd.ch>
2022-05-25 10:50:01 +0200

    CORS?

--- src/main/java/ch/fdb/zythopedia/ZythopediaApplication.java
+++ src/main/java/ch/fdb/zythopedia/ZythopediaApplication.java
@@ -2,12 +2,35 @@ package ch.fdb.zythopedia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class ZythopediaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZythopediaApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+		var source = new UrlBasedCorsConfigurationSource();
+		var config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(Arrays.asList("*"));
+		config.setAllowedMethods(Collections.singletonList("*"));
+		config.setAllowedHeaders(Collections.singletonList("*"));
+		source.registerCorsConfiguration("/**", config);
+		var bean = new FilterRegistrationBean<>(new CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
 	}
 
 }
