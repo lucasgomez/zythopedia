d7b627f8fd56fe62a15dcfc1d05f0ae23fa2af2e
Lucas Gomez <lucas.gomez@vd.ch>
2022-05-25 15:51:23 +0200

    Remove old front

--- front/old/src/data/apiConfig.js
+++
@@ -1,6 +0,0 @@
-let backendHost;
-
-  //backendHost = 'http://drinks.elveteek.ch';
-  backendHost = process.env.REACT_APP_BACKEND_HOST || 'http://localhost:8080';
-
-export const API_ROOT = `${backendHost}`;
